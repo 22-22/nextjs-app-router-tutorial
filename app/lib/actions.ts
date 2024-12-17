df812568c796bd3e94083f5fd44996e9fbee8fe8
Yuliya Kogol <kogol.julia@gmail.com>
2024-12-17 10:07:46 +0100

    fix: fix lint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,7 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     } catch (error) {
         console.error({
             message: 'Database Error: Failed to Create Invoice.',
-        });
+        }, error);
     }
 
     revalidatePath('/dashboard/invoices');
@@ -82,7 +82,7 @@ export async function updateInvoice(id: string, formData: FormData) {
         WHERE id = ${id}
       `;
     } catch (error) {
-        console.error({ message: 'Database Error: Failed to Update Invoice.' });
+        console.error({ message: 'Database Error: Failed to Update Invoice.' }, error);
     }
 
     revalidatePath('/dashboard/invoices');
@@ -95,6 +95,6 @@ export async function deleteInvoice(id: string) {
         revalidatePath('/dashboard/invoices');
         console.log({ message: 'Deleted Invoice.' });
     } catch (error) {
-        console.error({ message: 'Database Error: Failed to Delete Invoice.' })
+        console.error({ message: 'Database Error: Failed to Delete Invoice.' }, error)
     }
 }
